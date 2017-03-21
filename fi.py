0111831e569a5b6360b7906971acd500f59fa686
gomsister <rhdmsql100@naver.com>
2017-03-21 14:16:56 +0900

    test

--- fi.py
+++ fi.py
@@ -1,6 +1,8 @@
 from collections import Counter
 from konlpy.tag import Twitter
 
+keywords = []
+
 def keyword_extract(filename): 
    f = open(filename)
    data = f.read()
@@ -12,7 +14,12 @@ def keyword_extract(filename):
    words = count.most_common(40)
 
    keyword = words[0:3]
+
+   for i in keyword:
+   	word_freq_bid = [i[0],i[1],filename[:-4]]
+   	keywords.append(word_freq_bid)
+
    f.close()
-   return keyword
+   return keywords
 
 print(keyword_extract('1.txt'))
--- py-scripts/test.py
+++ py-scripts/test.py
@@ -1,12 +1,14 @@
-import sys
+import sys, codecs
 
 fileNames =  sys.argv
 
 from collections import Counter
 from konlpy.tag import Twitter
- 
-def keyword_extract(filename): 
-   f = open(filename)
+
+keywords = []
+
+def keywords_extract(filename): 
+   f = codecs.open('./reviews/'+ filename,"r", "utf-8")
    data = f.read()
 
    nlp = Twitter()
@@ -16,7 +18,15 @@ def keyword_extract(filename):
    words = count.most_common(40)
 
    keyword = words[0:3]
-   f.close()
-   return keyword
 
-print(keyword_extract(fileNames[1]))
+   for i in keyword:
+   	word_freq_bid = [i[0],i[1],filename[:-4]]
+   	keywords.append(word_freq_bid)
+
+   f.close()
+   return keywords
+
+for n in range(1, 6):
+	keywords_extract(fileNames[int(n)])
+
+print(keywords)
